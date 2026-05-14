e0bf61fb13683a1d62890f20ccf562311426d865
Hyobeen-Park <amanda0109@naver.com>
2026-05-14 20:10:14 +0900

    [FEAT/#3] 타이포 토큰 등록

---
+++ mds/src/main/java/org/sopt/official/mds/theme/MdsTypographyScheme.kt
@@ -0,0 +1,116 @@
+package org.sopt.official.mds.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import org.sopt.official.mds.foundation.base.MdsBaseTypography
+import org.sopt.official.mds.foundation.base.MdsFontFamily
+import org.sopt.official.mds.foundation.semantic.MdsTypography
+
+val MdsTypographyScheme = MdsTypography(
+    heading = MdsTypography.Heading(
+        h1 = TextStyle(
+            fontFamily = MdsFontFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = MdsBaseTypography.size32,
+            lineHeight = MdsBaseTypography.line48,
+            letterSpacing = MdsBaseTypography.spacingDefault
+        ),
+        h2 = TextStyle(
+            fontFamily = MdsFontFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = MdsBaseTypography.size24,
+            lineHeight = MdsBaseTypography.line36,
+            letterSpacing = MdsBaseTypography.spacingDefault
+        ),
+        h3 = TextStyle(
+            fontFamily = MdsFontFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = MdsBaseTypography.size20,
+            lineHeight = MdsBaseTypography.line30,
+            letterSpacing = MdsBaseTypography.spacingDefault
+        ),
+        h4 = TextStyle(
+            fontFamily = MdsFontFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = MdsBaseTypography.size16,
+            lineHeight = MdsBaseTypography.line24,
+            letterSpacing = MdsBaseTypography.spacingWide
+        )
+    ),
+    title = MdsTypography.Title(
+        t1 = TextStyle(
+            fontFamily = MdsFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = MdsBaseTypography.size28,
+            lineHeight = MdsBaseTypography.line42,
+            letterSpacing = MdsBaseTypography.spacingDefault
+        ),
+        t2 = TextStyle(
+            fontFamily = MdsFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = MdsBaseTypography.size24,
+            lineHeight = MdsBaseTypography.line36,
+            letterSpacing = MdsBaseTypography.spacingDefault
+        ),
+        t3 = TextStyle(
+            fontFamily = MdsFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = MdsBaseTypography.size20,
+            lineHeight = MdsBaseTypography.line30,
+            letterSpacing = MdsBaseTypography.spacingDefault
+        ),
+        t4 = TextStyle(
+            fontFamily = MdsFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = MdsBaseTypography.size16,
+            lineHeight = MdsBaseTypography.line24,
+            letterSpacing = MdsBaseTypography.spacingWide
+        )
+    ),
+    body = MdsTypography.Body(
+        b1 = TextStyle(
+            fontFamily = MdsFontFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = MdsBaseTypography.size16,
+            lineHeight = MdsBaseTypography.line26,
+            letterSpacing = MdsBaseTypography.spacingWide
+        ),
+        b2 = TextStyle(
+            fontFamily = MdsFontFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = MdsBaseTypography.size14,
+            lineHeight = MdsBaseTypography.line22,
+            letterSpacing = MdsBaseTypography.spacingWide
+        )
+    ),
+    label = MdsTypography.Label(
+        l1 = TextStyle(
+            fontFamily = MdsFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = MdsBaseTypography.size18,
+            lineHeight = MdsBaseTypography.line24,
+            letterSpacing = MdsBaseTypography.spacingDefault
+        ),
+        l2 = TextStyle(
+            fontFamily = MdsFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = MdsBaseTypography.size16,
+            lineHeight = MdsBaseTypography.line22,
+            letterSpacing = MdsBaseTypography.spacingDefault
+        ),
+        l3 = TextStyle(
+            fontFamily = MdsFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = MdsBaseTypography.size14,
+            lineHeight = MdsBaseTypography.line18,
+            letterSpacing = MdsBaseTypography.spacingDefault
+        ),
+        l4 = TextStyle(
+            fontFamily = MdsFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = MdsBaseTypography.size12,
+            lineHeight = MdsBaseTypography.line16,
+            letterSpacing = MdsBaseTypography.spacingDefault
+        )
+    )
+)
--- mds/src/main/java/org/sopt/official/mds/theme/SoptTheme.kt
+++ mds/src/main/java/org/sopt/official/mds/theme/SoptTheme.kt
@@ -6,25 +6,37 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.staticCompositionLocalOf
 import org.sopt.official.mds.foundation.semantic.MdsColors
+import org.sopt.official.mds.foundation.semantic.MdsTypography
 
 object SoptTheme {
     val colors: MdsColors
         @Composable
         @ReadOnlyComposable
         get() = LocalSoptColors.current
+
+    val typography: MdsTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalSoptTypography.current
 }
 
 private val LocalSoptColors = staticCompositionLocalOf<MdsColors> {
     error("No SoptColors provided")
 }
 
+private val LocalSoptTypography = staticCompositionLocalOf<MdsTypography> {
+    error("No SoptTypography provided")
+}
+
 @Composable
 fun ProvideSoptColorsAndTypography(
     colors: MdsColors,
+    typography: MdsTypography,
     content: @Composable () -> Unit
 ) {
     CompositionLocalProvider(
         LocalSoptColors provides colors,
+        LocalSoptTypography provides typography,
         content = content
     )
 }
@@ -32,7 +44,8 @@ fun ProvideSoptColorsAndTypography(
 @Composable
 fun SoptTheme(darkTheme: Boolean = false, content: @Composable () -> Unit) {
     val colors = DarkMdsColorScheme
-    ProvideSoptColorsAndTypography(colors) {
+    val typography = MdsTypographyScheme
+    ProvideSoptColorsAndTypography(colors, typography) {
         MaterialTheme(content = content)
     }
 }
