bc9952a0faaf1ff4ebe2359d8d282c8f48fb9d8b
Hyobeen-Park <amanda0109@naver.com>
2026-06-04 19:38:55 +0900

    [ADD/#15] add coil3

--- mds/build.gradle.kts
+++ mds/build.gradle.kts
@@ -49,4 +49,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
 }
