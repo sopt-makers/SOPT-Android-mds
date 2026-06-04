141ba0cdc2de34ed99e30387b23520bb5111c273
Hyobeen-Park <amanda0109@naver.com>
2026-06-04 18:24:59 +0900

    [ADD/#13] add MdsRadius

---
+++ mds/src/main/java/org/sopt/official/mds/foundation/semantic/MdsRadius.kt
@@ -0,0 +1,30 @@
+package org.sopt.official.mds.foundation.semantic
+
+import org.sopt.official.mds.foundation.base.size0
+import org.sopt.official.mds.foundation.base.size10
+import org.sopt.official.mds.foundation.base.size12
+import org.sopt.official.mds.foundation.base.size14
+import org.sopt.official.mds.foundation.base.size16
+import org.sopt.official.mds.foundation.base.size2
+import org.sopt.official.mds.foundation.base.size20
+import org.sopt.official.mds.foundation.base.size24
+import org.sopt.official.mds.foundation.base.size32
+import org.sopt.official.mds.foundation.base.size4
+import org.sopt.official.mds.foundation.base.size6
+import org.sopt.official.mds.foundation.base.size8
+
+object MdsRadius {
+    val r0 = size0
+    val r2 = size2
+    val r4 = size4
+    val r6 = size6
+    val r8 = size8
+    val r10 = size10
+    val r12 = size12
+    val r14 = size14
+    val r16 = size16
+    val r20 = size20
+    val r24 = size24
+    val r32 = size32
+    val full = org.sopt.official.mds.foundation.base.full
+}
