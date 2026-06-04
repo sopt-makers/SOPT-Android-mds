bd93fabfb810544af64fae3413059a7688d0199d
Hyobeen-Park <amanda0109@naver.com>
2026-06-04 18:24:50 +0900

    [ADD/#13] add MdsSpacing

---
+++ mds/src/main/java/org/sopt/official/mds/foundation/semantic/MdsSpacing.kt
@@ -0,0 +1,47 @@
+package org.sopt.official.mds.foundation.semantic
+
+import org.sopt.official.mds.foundation.base.size0
+import org.sopt.official.mds.foundation.base.size10
+import org.sopt.official.mds.foundation.base.size12
+import org.sopt.official.mds.foundation.base.size120
+import org.sopt.official.mds.foundation.base.size14
+import org.sopt.official.mds.foundation.base.size16
+import org.sopt.official.mds.foundation.base.size160
+import org.sopt.official.mds.foundation.base.size2
+import org.sopt.official.mds.foundation.base.size20
+import org.sopt.official.mds.foundation.base.size24
+import org.sopt.official.mds.foundation.base.size28
+import org.sopt.official.mds.foundation.base.size32
+import org.sopt.official.mds.foundation.base.size36
+import org.sopt.official.mds.foundation.base.size4
+import org.sopt.official.mds.foundation.base.size40
+import org.sopt.official.mds.foundation.base.size48
+import org.sopt.official.mds.foundation.base.size6
+import org.sopt.official.mds.foundation.base.size64
+import org.sopt.official.mds.foundation.base.size72
+import org.sopt.official.mds.foundation.base.size8
+import org.sopt.official.mds.foundation.base.size80
+
+object MdsSpacing {
+    val s0 = size0
+    val s2 = size2
+    val s4 = size4
+    val s6 = size6
+    val s8 = size8
+    val s10 = size10
+    val s12 = size12
+    val s14 = size14
+    val s16 = size16
+    val s20 = size20
+    val s24 = size24
+    val s28 = size28
+    val s32 = size32
+    val s36 = size36
+    val s40 = size40
+    val s48 = size48
+    val s64 = size64
+    val s72 = size72
+    val s80 = size80
+    val s120 = size120
+    val s160 = size160
+}
