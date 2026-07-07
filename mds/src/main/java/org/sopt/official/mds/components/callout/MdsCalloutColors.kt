d35d45ac614868090b7e5488934d03af0039e9e0
Hyobeen Park <98209004+Hyobeen-Park@users.noreply.github.com>
2026-07-08 05:59:55 +0900

    [FEAT/#10] callout 구현 (#41)

---
+++ mds/src/main/java/org/sopt/official/mds/components/callout/MdsCalloutColors.kt
@@ -0,0 +1,12 @@
+package org.sopt.official.mds.components.callout
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Color
+
+@Immutable
+internal data class MdsCalloutColors(
+    val background: Color,
+    val border: Color,
+    val text: Color,
+    val icon: Color
+)
